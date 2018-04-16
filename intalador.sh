ab99b413b329a71bc8a2170d1ef82827d52b4dd0
marcuxo <marcourrutiam@gmail.com>
2018-04-15 23:26:01 -0300

    comit dos

--- intalador.sh
+++ intalador.sh
@@ -5,6 +5,14 @@ yum install httpd
 yum systemctl start httpd.service
 yum install php
 systemctl restart httpd.service
+yum install wget
+wget http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+yum install mysql57-community-release-el7-7.noarch.rpm
+yum install mysql-community-server
+service mysql start
+service mysql status
+mysql --version
+echo Gracias Por instalar L.A.M.P
 sleep 1s
 exit
 
